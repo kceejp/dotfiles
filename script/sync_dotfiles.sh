eb5c10987356e1c722972a15fd2cbc8174edb666
KCeejp <elohssa@ryan.(none)>
2011-01-10 17:33:05 +0000

    global git config and gitconfig, unite configuration

--- script/sync_dotfiles.sh
+++ script/sync_dotfiles.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 # http://d.hatena.ne.jp/sugyan/20100602/1275414838
+cd $HOME/dotfiles/global
+echo $PWD
+for dotfile in .?*
+do
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+    then
+        ln -Fis "$PWD/$dotfile" $HOME
+    fi
+done
+
 cd $HOME/dotfiles
 echo $PWD
 for dotfile in .?*

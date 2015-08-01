773f4fb70d9754a5ef6d6b95634d63b8ba81d90b
KCeejp <stomachache4988@gmail.com>
2015-08-02 04:42:06 +0900

    add neovim & add fish key bindings

--- home/.config/fish/functions/fish_user_key_bindings.fish
+++ home/.config/fish/functions/fish_user_key_bindings.fish
@@ -8,12 +8,24 @@ function fish_user_key_bindings
     # Turn Vi mode on
     fish_vi_key_bindings
 
-    # Ctrl-h: peco
-    bind -e -M insert \ch
-    bind -M insert \ch peco_select_history
-
     # Ctrl-c: switch to the visual mode
     bind -e -M insert \cc
     bind -M insert -m default \cc force-repaint
+
+    # Ctrl-c: clear line nad back to insert mode
+    bind -e \cc
+    bind -m insert \cc kill-whole-line force-repaint
+
+    # Ctrol-l: to clear screen
+    bind -e -M insert \cl
+    bind -M insert \cl clear
+
+    # Need this for some reasons...
+    bind -e -M insert -k backspace
+    bind -M insert -k backspace backward-delete-char
+
+    # Ctrl-h: peco
+    bind -e -M insert \ch
+    bind -M insert \ch "peco_select_history (commandline)"
 end
 
